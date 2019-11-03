702c8a6c8707a0fe1f01835455b5c464144e1eac
Robert Baxter <baxterrp@gmail.com>
2019-11-03 14:02:33 -0500

    added first api controller method - tested

---
+++ SimpleNorthwindsApi/SimpleNorthwindsApi/Controllers/CustomerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleNorthwindsApi.Controllers
+{
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet("/customers")]
+        public IActionResult GetAllCustomers()
+        {
+            return Ok();
+        }
+    }
+}

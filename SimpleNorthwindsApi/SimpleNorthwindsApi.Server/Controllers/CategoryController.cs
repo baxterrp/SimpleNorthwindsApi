e9310c7bdc0b63eaabf1a7774385ce9e0fe9dcc8
Robert Baxter <baxterrp@gmail.com>
2019-11-06 19:30:51 -0500

    added content to category controller

--- SimpleNorthwindsApi/SimpleNorthwindsApi.Server/Controllers/CategoryController.cs
+++ SimpleNorthwindsApi/SimpleNorthwindsApi.Server/Controllers/CategoryController.cs
@@ -1,6 +1,39 @@
-﻿namespace SimpleNorthwindsApi.Server.Controllers
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using SimpleNorthwindsApi.Common;
+using SimpleNorthwindsApi.Server.Services.Categories;
+
+namespace SimpleNorthwindsApi.Server.Controllers
 {
-    public class CategoryController 
+    public class CategoryController : ControllerBase
     {
+        ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService ?? throw new ArgumentNullException(nameof(categoryService));
+        }
+
+        [HttpGet("/categories")]
+        public IActionResult GetAllCategories() => Ok(_categoryService.GetAllCategories());
+
+        [HttpGet("/categories/{id}")]
+        public IActionResult FindCategoryById([FromRoute]string id) => Ok(_categoryService.FindCategoryById(id));
+
+        [HttpPost("/categories")]
+        public IActionResult AddNewCategory([FromBody]Category category)
+        {
+            _categoryService.AddNewCategory(category);
+
+            return Ok();
+        }
+
+        [HttpDelete("/categories/{id}")]
+        public IActionResult DeleteCategory([FromRoute]string id)
+        {
+            _categoryService.DeleteCategory(id);
+
+            return Ok();
+        }
     }
 }
